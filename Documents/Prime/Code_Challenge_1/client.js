b3c64b510cc86cfa4a82d3737de823c94c3647f3
Antonio Rice <ricex407@gmail.com>
2017-06-26 14:34:08 -0500

    complete

---
+++ Documents/Prime/Code_Challenge_1/client.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+//Create a <button> element on the DOM, you may do
+//this in jQuery, or write it directly in the HTML
+//code. Have the button text read "Generate".
+
+
+//Using jQuery, append a <div> element when you click the button.
+
+var  numClicked = 0;
+$('#testButton').on('click', function(){
+numClicked +=1;
+$('.container').append('<div></div>');
+$('div').append('<p>' +(numClicked) + '</p>')
+$('div').append('<button id="swapButton">' + "Swap" +'</button>' + '<button id="deleteButton">' + "Delete" +'</button>');
+//$('div').append('<button id="deleteButton">' + "Delete" +'</button>');
+});
+
+
+
+}); //end of main ready function
